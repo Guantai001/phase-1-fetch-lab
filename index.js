3908936f3ffeae08f1f9f21ea9ebebed9db57f33
guantai <jpguantai@gmail.com>
2022-11-30 07:20:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+
+  return fetch('https://anapioficeandfire.com/api/books')
+   .then (function(response) {
+      return response.json();
+   })
+    .then (function(json) {
+      renderBooks(json);
+    });
+
+  }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +23,8 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+
   fetchBooks();
+
+
 });
